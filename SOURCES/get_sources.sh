889b2eaedaaffa13c01e066f106b7cad751b4977
Ami Rahav <arahav@boundlessgeo.com>
2016-04-24 16:28:22 -0400

    Adding geonode-geoserver.spec

--- SOURCES/get_sources.sh
+++ SOURCES/get_sources.sh
@@ -14,6 +14,8 @@ srcs+=("https://s3.amazonaws.com/boundlessps-public/geoshape/src/gdal-2.0.2.tar.
 srcs+=("https://s3.amazonaws.com/boundlessps-public/geoshape/src/postgis-2.2.2.tar.gz")
 srcs+=("https://s3.amazonaws.com/boundlessps-public/geoshape/src/apache-tomcat-8.0.33.tar.gz")
 srcs+=("https://s3.amazonaws.com/boundlessps-public/geoshape/src/mod_xsendfile-0.12.tar.bz2")
+srcs+=("https://s3.amazonaws.com/boundlessps-public/geoshape/src/geoserver/2.8/geoserver.war")
+srcs+=("https://s3.amazonaws.com/boundlessps-public/geoshape/src/geoserver_data-geogig_od3.zip")
 
 if [ $version == 7 ];then
   srcs+=("https://s3.amazonaws.com/boundlessps-public/geoshape/src/MrSID_DSDK-9.5.1.4427-linux.x86-64.gcc48.tar.gz")
